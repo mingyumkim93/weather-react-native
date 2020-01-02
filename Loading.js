3ac6e0429a9c711b9c165d0eecf6904909325f0c
mingyumkim93 <mingyumkim93@gmail.com>
2020-01-02 12:57:03 +0200

    loading page

---
+++ Loading.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, Text, View} from "react-native";
+
+export default function Loading(){
+    return <View style={styles.container}>
+        <Text style={styles.text}>Getting the weather for your location</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1, 
+        justifyContent: "flex-end",
+        paddingHorizontal: 30,
+        paddingVertical: 100,
+        backgroundColor: "#37c4bd"
+    },
+    text:{
+        color: "black",
+        fontSize: 30
+    }
+})
