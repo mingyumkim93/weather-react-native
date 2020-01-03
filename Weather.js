9c6a94eaa7b0b9b986e3714aa2ae40985764a81a
mingyumkim93 <mingyumkim93@gmail.com>
2020-01-03 11:12:07 +0200

    styling

--- Weather.js
+++ Weather.js
@@ -1,16 +1,61 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 
-export default function Weather({ temp }) {
+const weatherOptions = {
+    Thunderstorm:{
+        iconName:"ios-thunderstorm",
+        gradient: ["#94999c","#282829"],
+        title:"Thunderstorm",
+        subtitle:"Stay inside"
+    },
+    Drizzle:{
+        iconName:"md-rainy",
+        gradient: ["#6d90ab","#0c3352"],
+        title:"Drizzle",
+        subtitle:"Awful weather"
+    },
+    Rain:{
+        iconName:"ios-rainy",
+        gradient: ["#7abecc","#21afcc"],
+        title:"Rain",
+        subtitle:"Take you umbrella"
+    },
+    Snow:{
+        iconName:"ios-snow",
+        gradient: ["#8e97d1","#1934e0"],
+        title:"Snow",
+        subtitle:"Snowwwwwwwww"
+    },
+    Clear:{
+        iconName:"ios-sunny",
+        gradient: ["#ebe9a0","#cc2735"],
+        title:"Clear",
+        subtitle:"Nice clear weather"
+    },
+    Clouds:{
+        iconName:"ios-cloudy",
+        gradient: ["#5d92b0","#1f506b"],
+        title:"Clouds",
+        subtitle:"Clouds . . ."
+    },
+}
+
+export default function Weather({ temp, condition }) {
   return (
-    <View style={styles.container}>
+    <LinearGradient colors={weatherOptions[condition].gradient} style={styles.container}>
+    <StatusBar barStyle="light-content"/>
     <View style={styles.halfContainer}>
-    <MaterialCommunityIcons name="weather-lightning-rainy" size={150}/>
       <Text style={styles.temp}>{temp}°C</Text>
+      <Ionicons name={weatherOptions[condition].iconName} size={150} color="white"/>
     </View>
-    <View style={styles.halfContainer}></View>
+    <View style={{...styles.halfContainer, ...styles.textContainer}}>
+      <Text style={styles.Title}>{weatherOptions[condition].title}</Text>
+      <Text style={styles.Subtitle}>{weatherOptions[condition].subtitle}</Text>
     </View>
+    <View><Text style={styles.copyright}>Created by Mingyum Kim(Migu)</Text></View>
+    </LinearGradient>
   );
 }
 
@@ -21,11 +66,30 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   temp:{
-      fontSize:50
+      fontSize:50,
+      color:"white"
   },
   halfContainer:{
       flex:1,
       justifyContent:"center",
       alignItems:"center",
+  },
+  textContainer:{
+    paddingHorizontal: 20,
+    alignItems:"flex-start"
+  },
+  Title:{
+    fontSize: 50,
+    fontWeight:"600",
+    color:"white"
+  },
+  Subtitle:{
+    fontSize: 30,
+    fontWeight:"300",
+    color:"white"
+  },
+  copyright:{
+    textAlign:"left",
+    textDecorationLine:"underline"
   }
 });
